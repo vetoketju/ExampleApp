f16aad5a7c0e3e748c0cbf9d884340ec2d9ef39f
Ville Valta <ville.valta@qvik.fi>
2017-08-14 14:05:21 +0300

    Cleaner ApiSercvice

--- app/src/main/java/com/villevalta/exampleapp/network/service/ImgurApiService.java
+++ app/src/main/java/com/villevalta/exampleapp/network/service/ImgurApiService.java
@@ -21,39 +21,37 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class ImgurApiService {
 
-    private static final String IMGUR_API_BASE_URL = "https://api.imgur.com/3/";
-
     OkHttpClient httpClient;
     ImgurApiServiceInterface service;
 
 
-
-
     public ImgurApiService() {
 
-        // Luodaan OkHttpClient instanssi. Imgur tapauksessa jokaiseen http kutsuun pitää lisätä authorization header. Sitä varten tehdään Interceptor, joka laittaa sen jokaiseen kutsuun.
-        httpClient = new OkHttpClient.Builder().addNetworkInterceptor(new Interceptor() {
+
+        Interceptor authInterceptor = new Interceptor() {
             @Override
             public Response intercept(Chain chain) throws IOException {
                 Request request = chain.request();
                 request = request.newBuilder().addHeader("Authorization", "Client-ID " + BuildConfig.IMGUR_CLIENT_ID).build();
                 return chain.proceed(request);
             }
-        })
-                // Säädetään timeoutit isommiksi. Oletukset eivät yleensä riitä, koska julkiset rajapinnat ovat hitaita:
+        };
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+
+
+        // Luodaan OkHttpClient instanssi. Imgur tapauksessa jokaiseen http kutsuun pitää lisätä authorization header. Sitä varten tehdään Interceptor, joka laittaa sen jokaiseen kutsuun.
+        httpClient = new OkHttpClient.Builder()
+                .addNetworkInterceptor(authInterceptor)
+                .addNetworkInterceptor(loggingInterceptor)
                 .connectTimeout(1, TimeUnit.MINUTES)
                 .readTimeout(1, TimeUnit.MINUTES)
                 .writeTimeout(1, TimeUnit.MINUTES)
                 .build();
 
-        if(BuildConfig.DEBUG){
-            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
-            httpClient = httpClient.newBuilder().addInterceptor(loggingInterceptor).build();
-        }
-
         service = new Retrofit.Builder()
-                .baseUrl(IMGUR_API_BASE_URL)
+                .baseUrl(BuildConfig.BASE_URL)
                 .client(httpClient)
                 .addConverterFactory(JacksonConverterFactory.create())
                 .build()
